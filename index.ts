8b8ab37cd7b2e4c329620de77cf0c71ab1ab3b23
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-31 00:29:56 +0300

    fin1

--- index.ts
+++ index.ts
@@ -9,7 +9,6 @@ import { bufferTime } from 'rxjs/operators';
 const srcInterval$ = interval(500);
 //After 2 seconds have passed, emit buffered values as an array
 const example = srcInterval$.pipe(bufferTime(2000));
-//Print values to console
 //ex. output [0,1,2]...[3,4,5,6]
 const subscribe = example.subscribe((val) =>
   console.log('Buffered with Time:', val)
