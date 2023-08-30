1516051bdcb0d3a5eade3548ef8ca57e0c3597a6
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-31 00:25:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-bufferTime-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-5rn9rl)
---
+++ index.ts
@@ -0,0 +1,13 @@
+// RxJS v6+
+import { interval } from 'rxjs';
+import { bufferTime } from 'rxjs/operators';
+
+//Create an observable that emits a value every 500ms
+const source = interval(500);
+//After 2 seconds have passed, emit buffered values as an array
+const example = source.pipe(bufferTime(2000));
+//Print values to console
+//ex. output [0,1,2]...[3,4,5,6]
+const subscribe = example.subscribe((val) =>
+  console.log('Buffered with Time:', val)
+);
