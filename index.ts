4a11b9c7e8b25739203d3161d50ee1c75daecd56
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-31 00:27:16 +0300

    fin

--- index.ts
+++ index.ts
@@ -2,10 +2,13 @@
 import { interval } from 'rxjs';
 import { bufferTime } from 'rxjs/operators';
 
+// Example 1: Buffer for 2 seconds
+// https://www.learnrxjs.io/learn-rxjs/operators/transformation/buffertime
+
 //Create an observable that emits a value every 500ms
-const source = interval(500);
+const srcInterval$ = interval(500);
 //After 2 seconds have passed, emit buffered values as an array
-const example = source.pipe(bufferTime(2000));
+const example = srcInterval$.pipe(bufferTime(2000));
 //Print values to console
 //ex. output [0,1,2]...[3,4,5,6]
 const subscribe = example.subscribe((val) =>
